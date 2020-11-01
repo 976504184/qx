221ac970bffe89b15349555f24658b6b1c3b7520
hausthy <hausthy@163.com>
2020-11-01 12:26:22 +0800

    ui

--- script/rewrite/xxys.js
+++ script/rewrite/xxys.js
@@ -3,6 +3,7 @@
 小小影视 unlock Vip&免广告
 QX:
 https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index) url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xxys.js
+
 Surge:
 http-response https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/xxys.js
 MITM = *.xxjjappss.com,*.huaerdadi.com
