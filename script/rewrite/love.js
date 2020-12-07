f7d487b5801645769730265412e157aa8405b5c1
976504184 <45617229+976504184@users.noreply.github.com>
2020-12-07 22:48:29 +0800

    Update love.js

--- script/rewrite/love.js
+++ script/rewrite/love.js
@@ -2,22 +2,20 @@
 更新：2020-11-01
 破解收费
 QX:
-https:\/\/lovezbapi\.zjjieneng\.cn\/api\/public\/?service=* url script-request-header https://raw.githubusercontent.com/976504184/qx/master/script/rewrite/love.js
+^https:\/\/lovezbapi\.zjjieneng\.cn\/api\/public\/\?service=.* url script-request-header https://raw.githubusercontent.com/976504184/qx/master/script/rewrite/love.js
 MITM = lovezbapi.zjjieneng.cn
 */
 
 
 let obj = JSON.parse($response.body);
 
-console.log(obj)
-
 if (obj.data.info) {
 	for(var info in obj.data.info){
-           info.type="0";
-           info.type_val="0";
+           info.type = "0";
+           info.type_val = "0";
     }
 }
-console.log(obj)
+console.log(JSON.stringify(obj))
 $done({
 	body: JSON.stringify(obj)
 });
