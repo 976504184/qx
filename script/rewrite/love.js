4ce6247103de00e4e1af3e8d5b4c3ad573c69452
976504184 <45617229+976504184@users.noreply.github.com>
2020-12-08 10:01:33 +0800

    Update love.js
    fix

--- script/rewrite/love.js
+++ script/rewrite/love.js
@@ -9,9 +9,9 @@ MITM = lovezbapi.zjjieneng.cn
 let obj = JSON.parse($response.body);
 
 if (obj.data.info) {
-	for(var info in obj.data.info){
-           obj.data.info[info].type = "0";
-           obj.data.info[info].type_val = "0";          
+	for(const info of obj.data.info){
+           info.type = "0";
+           info.type_val = "0";          
     }
 }
 $done({
