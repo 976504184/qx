6f61f9664bc16cf54a43bc702090ff9013709f42
976504184 <45617229+976504184@users.noreply.github.com>
2021-01-22 20:31:57 +0800

    Update test.js

--- tz/test.js
+++ tz/test.js
@@ -1,1 +1,8 @@
 
+[rewrite_local]
+#桃子视频解锁会员去广告
+^http:\/\/api\.taozishipin02\.xyz\/fast-cloud\/user\/info url script-response-body https://raw.githubusercontent.com/976504184/qx/master/tz/tz.js
+^http:\/\/api\.taozishipin02\.xyz\/fast-cloud\/ads\/fetch url script-response-body https://raw.githubusercontent.com/976504184/qx/master/tz/taozi.js
+
+[mitm]
+hostname = api.taozishipin02.xyz
