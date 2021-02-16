476cccb06cfc2cb8228ab9d2871f0e9a6bb8aa33
976504184 <45617229+976504184@users.noreply.github.com>
2021-02-16 23:56:14 +0800

    Update love.js

--- script/rewrite/love.js
+++ script/rewrite/love.js
@@ -2,7 +2,7 @@
 更新：2021-02-16
 破解收费
 QX:
-^https:\/\/lovezbapi\.jf-mrx\.com\.cn\/api\/public\/\?.*&service=Home\.(?!getConfig).* url script-response-body https://raw.githubusercontent.com/976504184/qx/master/script/rewrite/love.js
+^https:\/\/lovezbapi\.jf-mrx\.com\.cn\/api\/public\/\?.*&service=(Home|Live)\.(?!getConfig).* url script-response-body https://raw.githubusercontent.com/976504184/qx/master/script/rewrite/love.js
 MITM = lovezbapi.jf-mrx.com.cn
 */
 
@@ -13,8 +13,10 @@ if (obj.data.info) {
         info.type = "0";
         info.type_val = "0";
         info.level = "2";
+        info.real_type = "0";
+        info.type_msg = "";
     }
-   //console.log(obj.data.info);
+    //console.log(obj.data.info);
 }
 $done({
     body: JSON.stringify(obj)
